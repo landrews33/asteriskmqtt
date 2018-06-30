eef1abd760fe13b2b1d2c034cf1d5419d728d55b
Lance Andrews <landrews33@users.noreply.github.com>
2018-06-30 08:13:39 +0100

    Updates

--- asterisk.js
+++ asterisk.js
@@ -1,21 +1,36 @@
 var mqtt = require('mqtt');
 var config = require('./config.json');
 var config_schema = require('./schema_config.json');
+var winston = require('winston');
+var consoleLog = new (winston.Logger) ({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({filename: 'asteriskmqtt.log', maxsize:1048576, maxFiles:10,tailable:true,zippedArchive:true})
+  ]
+});
+var mqttLog = new (winston.Logger) ({
+  transports:[new (winston.transports.File)({filename: 'mqtt.log', maxsize:1048576, maxFiles:10,tailable:true,zippedArchive:true})]
+});
+var asteriskLog = new (winston.Logger) ({
+  transports:[new (winston.transports.File)({filename: 'asterisk.log', maxsize:1048576, maxFiles:10,tailable:true,zippedArchive:true})]
+});
 var ami = {};
 var mqttClient;
 var channelTracker = {};
+var eventTracker = {};
 var channelMap={};
 var lines;
 
+
 function startBridge(config)
 {	
-	console.log("Connecting to Asterisk");
+	consoleLog.info("Connecting to Asterisk");
 	ami = new require('asterisk-manager')(config.asterisk.port, config.asterisk.host, config.asterisk.user, config.asterisk.password, true);
 	ami.on('connect', function() {
-		console.log("Asterisk Connected");
+		consoleLog.info("Asterisk Connected");
 	});
 	ami.keepConnected();
-	console.log("Connecting to MQTT");
+	consoleLog.info("Connecting to MQTT");
 	mqttClient = mqtt.connect(config.mqtt.url, {"username":config.mqtt.username,"password":config.mqtt.password});
 
 	registerAMIEventHandlers(ami);
@@ -29,7 +44,7 @@ function registerMQTTEventHandlers(mqttClient,ami)
 		for (var i = 0; i < lines.length; i++) {
 			for(var j=0; j< lines[i].dids.length;j++)
 			{
-				console.log("Publishing Presence for " + lines[i].dids[j].id );
+				mqttLog.info("Publishing Presence for " + lines[i].dids[j].id );
 				mqttClient.publish(lines[i].dids[j].status_topic, JSON.stringify(lines[i].dids[j]));
 				mqttClient.publish("discovery/announce", JSON.stringify(lines[i].dids[j]));
 			}
@@ -42,21 +57,24 @@ function registerMQTTEventHandlers(mqttClient,ami)
 	        for (var i = 0; i < lines.length; i++) {
                         for(var j=0; j<lines[i].dids.length;j++)
 			{
-				console.log("Publishing State for " + lines[i].dids[j].status_topic);
+				mqttLog.info("Publishing State for " + lines[i].dids[j].status_topic);
 				mqttClient.publish(lines[i].dids[j].status_topic, '{"state":"idle"}', {"qos":2});
 				mqttClient.subscribe(lines[i].dids[j].command_topic);
 			}
 		}
 	}
-
+	mqttClient.on('error', function()
+	{
+		mqttLog.info('Error connecting to MQTT');
+	});
 	mqttClient.on('connect', publishStateAndSubscribe);
-	mqttClient.on('disconnect', function() { console.log("Disconnected")})
-	setInterval(publishPresence, 300000);
+	mqttClient.on('disconnect', function() { consoleLog.info("Disconnected from MQTT Broker")})
+	setInterval(publishPresence, 1800000);
 
 
 	mqttClient.on('message', function (topic, message) {
-		console.log("Topic" + topic);
-		console.log("Message" + message);	
+		mqttLog.info("Topic" + topic);
+		mqttLog.info("Message" + message);	
 
 	});
 }
@@ -66,9 +84,10 @@ function registerAMIEventHandlers(ami)
 {
         var singleEvent = config.singleeventpercall;
 	var callInbound = function(event) {
-		if(singleEvent && channelTracker.hasOwnProperty(event.linkedid))
+                asteriskLog.info(event);
+		if(singleEvent && eventTracker.hasOwnProperty(event.linkedid))
 		{
-			console.log("Channel " + event.linkedid + "already seen - ignoring");
+			consoleLog.info("Unique Event ID " + event.linkedid + "already seen - ignoring");
 			return;
 		}
 		var callEvent = {};
@@ -79,21 +98,28 @@ function registerAMIEventHandlers(ami)
 		callEvent.identifier=event.channel
 		callEvent.extendedInfo=event
 		//Todo: Include timestamp
-		channelTracker[event.uniqueid] = {"status":"ringing"};
-		var clearChannel = function()
+		eventTracker[event.uniqueid] = {"status":"ringing"};
+		var clearEvent = function()
 		{
-			if(channelTracker.hasOwnProperty(event.linkedid) && channelTracker[event.linkedid].status == "ringing")
+			if(eventTracker.hasOwnProperty(event.linkedid))
 			{
-				channelTracker[event.linkedid].status = "orphan";
-				setTimeout(clearChannel, 120000);
-			} else if (channelTracker.hasOwnProperty(event.linkedid) && channelTracker[event.linkedid].status == "orphan")
-			{
-				delete channelTracker[event.linkedid];
+				delete eventTracker[event.linkedid];	
 			}
 		}
+		var clearChannel = function()
+		{
+			if(channelTracker.hasOwnProperty(event.channel) && channelTracker[event.channel].status == "ringing")
+			{
+				channelTracker[event.channel].status = "orphan";
+				setTimeout(clearChannel, 120000);
+			} else if (channelTracker.hasOwnProperty(event.channel) && channelTracker[event.channel].status == "orphan")
+			{
+				delete channelTracker[event.channel];
+			}
+		}
+		setTimeout(clearEvent, 120000);
 		setTimeout(clearChannel, 120000);
-		console.log('Publishing Dial Event')
-		console.log(event);
+		consoleLog.info('Publishing Ringing Event to MQTT')
 		var lines = config.lines
 		//Channel is DialString-integer
 		var line = channelMap[event.channel.substring(0,event.channel.indexOf('-'))];
@@ -101,41 +127,65 @@ function registerAMIEventHandlers(ami)
 		{
 			var did = did_for_event(event);
 			//TODO: Detect outbound
-			console.log(did);
+			consoleLog.info("DID:" + did);
 		        mqttClient.publish(did.status_topic, '{"state":"ringing","direction":"inbound","callerid":"'+event.calleridnum+'","callername":"'+event.calleridname+'"}', {"qos":2});
-			channelTracker[event.uniqueid] = {};
-			channelTracker[event.uniqueid].did = did;
-			channelTracker[event.uniqueid].status = 'ringing';
+			channelTracker[event.channel] = {};
+			channelTracker[event.channel].did = did;
+			channelTracker[event.channel].callerid=event.calleridnum;
+			channelTracker[event.channel].callername=event.calleridname;
+			channelTracker[event.channel].direction='inbound';
+			channelTracker[event.channel].status = 'ringing';
 		}
 	}
 	
 	ami.on('dialbegin', callInbound);
+	var dialEnd = function(event)
+	{
+		asteriskLog.info(event);
+                if(event.uniqueid == event.linkedid)
+                {
+                        consoleLog.info("Primary Dial End on :" + JSON.stringify(channelTracker[event.channel]));
+                        if(typeof channelTracker[event.channel] == 'object')
+                        {
+				var channel = channelTracker[event.channel];
+                                consoleLog.info("Setting " + channel.did.did + " to onhook");
+				channelTracker[event.channel].status = "onhook";
+                                mqttClient.publish(channel.did.status_topic, '{"state":"missed_call","missed_callerid":"'+channel.calleridnum+'","missed_callername":"'+channel.calleridname+'}');
+                        }
+
+                }
+                consoleLog.info(event);
+	}
 	var hangUp = function(event)
 	{
+		asteriskLog.info(event);
 		if(event.uniqueid == event.linkedid)
 		{
-			console.log("Primary Hangup");
-			if(typeof channelTracker[event.uniqueid] == 'object')
+			consoleLog.info("Primary Hangup on :" + JSON.stringify(channelTracker[event.channel]));
+			if(typeof channelTracker[event.channel] == 'object')
 			{
-				console.log("Setting to idle");
-				mqttClient.publish(channelTracker[event.uniqueid].did.status_topic, '{"state":"idle"}');
+				//Need to identify missed calls.
+				consoleLog.info(channelTracker[event.channel].did);
+				consoleLog.info("Setting " + channelTracker[event.channel].did.did + " to onhook");
+                                mqttClient.publish(channelTracker[event.channel].did.status_topic, '{"state":"onhook"}');
+				delete channelTracker[event.channel];
 			}
 			
 		}
 		console.log(event);
 	}
 	//ami.on('pickup', call);
-	//ami.on('hangup', call);
+	ami.on('hangup', hangUp);
 	var extStatus = function(event)
 	{
-		console.log(event);
+                asteriskLog.log(event);
 	}
 	var dialComplete = function(event)
 	{
 		
 	}
-	ami.on('dialend', extStatus);
-	ami.on('dial', extStatus);
+	ami.on('dialend', dialEnd);
+//	ami.on('dial', extStatus);
 	ami.on('hangup', hangUp);
 	ami.on('join', extStatus);
 	ami.on('pickup', extStatus);
@@ -152,7 +202,7 @@ function did_for_event(event)
                {
                        for(var j=0; j<line.dids.length; j++)
                        {
-                               console.log(typeof line.dids[j].channel_cidprefix);
+                               consoleLog.info(typeof line.dids[j].channel_cidprefix);
                                if(typeof line.dids[j].channel_cidprefix == 'string')
                                {
                                         if(event.calleridname.indexOf(line.dids[j].channel_cidprefix) > -1)
@@ -171,6 +221,7 @@ try
 	var validate = require('jsonschema').validate;
 	validate(config,config_schema, {"throwError":true} ); 
 	lines = config.lines;
+	consoleLog.info("Creating Channel Map");
 	//Populate channel map so we can determine which line a Dial event belongs to.  
         for (var i = 0; i < lines.length; i++) {
 		channelMap[lines[i].channel] = lines[i];
